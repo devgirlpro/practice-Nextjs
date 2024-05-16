89e0eb21addac1f69f3e65b9681af6a04a2223f1
devgirlpro <agalidari@gmail.com>
2024-05-16 10:36:36 +0200

    diffrence between <a> & <Link>

---
+++ app/pages/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='flex flex-row justify-center m-4 text-[20px]' >
+      <h1>About page</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/pages/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='flex flex-row justify-center m-4 text-[20px]' >
+    <h1>COntact page</h1>
+  </div>
+  )
+}
+
+export default Contact
